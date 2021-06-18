6c1b2355bf259d124afa7533f68f82adb619490a
Zhou <peterzhouningbo@gmail.com>
2021-06-18 12:08:41 +0800

    Initial commit

---
+++ Example/XGLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  XGLib
+//
+//  Created by Zhou on 06/18/2021.
+//  Copyright (c) 2021 Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
